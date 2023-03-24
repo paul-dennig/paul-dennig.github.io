4fa46f9d15e25c03a9598ea01cd27ad6e6fc68ea
Paul Dennig <pauldennig@berkeley.edu>
2023-03-24 16:05:56 -0700

    styled toggle button

--- src/components/ToggleButton.js
+++ src/components/ToggleButton.js
@@ -1,14 +1,23 @@
 import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
+import moonIcon from '../assets/moon.png';
+import sunIcon from '../assets/sun.png'
 
-function ToggleButton() {
-  const { theme, toggleTheme } = useContext(ThemeContext);
+const ToggleButton = () => {
+    // load in data
+    const { theme, toggleTheme } = useContext(ThemeContext);
+    const sun = <img src={sunIcon} alt="sun" className='tog-icon inverted-icon'/>;
+    const moon = <img src={moonIcon} alt="moon" className='tog-icon'/>;
 
-  return (
-    <button onClick={toggleTheme}>
-      {theme === 'light' ? 'Dark Mode' : 'Light Mode'}
-    </button>
-  );
+    // create display variables
+    const icon = theme === 'light' ? moon : sun;
+    const label = theme === 'light' ? 'Dark Mode' : 'Light Mode';
+    
+    return (
+        <button className='toggle' onClick={toggleTheme}>
+            {icon}{label}
+        </button>
+    );
 }
-
+    
 export default ToggleButton;
