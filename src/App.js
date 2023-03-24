5774b90032806a999405e2e30707de424b88b562
Paul Dennig <pauldennig@berkeley.edu>
2023-03-23 20:29:52 -0700

    cleaned project hierarchy a bit

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
+// css + context
+import './styles/App.css';
+import { ThemeContext } from './contexts/ThemeContext'
+
 // pages
 import NavigationBar from './components/NavigationBar';
 import HomePage from './pages/';
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
-
-// help
-import './App.css';
-import { ThemeContext } from './contexts/ThemeContext'
 
 function App() {
   const [theme, setTheme] = useState("dark");
