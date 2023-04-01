240635b349d45fa192669bf807b155512c084b9e
Paul Dennig <pauldennig@berkeley.edu>
2023-04-01 00:17:11 -0700

    updated themes

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,7 +5,7 @@ const Contact = () => {
 		<div className='Contact'>
 			<h1>Please feel free to contact me!</h1>
 			<p>Email: pauldennig@berkeley.edu</p>
-			<p>LinkedIn: <a href="www.linkedin.com/in/paul-dennig-jr">www.linkedin.com/in/paul-dennig-jr</a></p>
+			<p>LinkedIn: <a href="http://www.linkedin.com/in/paul-dennig-jr">linkedin.com/in/paul-dennig-jr</a></p>
 		</div>
 	);
 };
